69e7bf006f8cfe88df4764bfc012439f78a65ffa
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-12 06:58:31 +0530

    done

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const ecpress = require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("Welcome to ranjan's app !");
+
+});
+app.listen(3000,function(){
+    console.log("app is listening to port 3000");
+});
