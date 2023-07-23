20a46a4a53de55b730b59f9da491f4e7c6b58f64
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-07-24 00:13:15 +0530

    Delete src directory

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send("Welcome to ranjan's app !");
-
-});
-app.listen(3000,function(){
-    console.log("app is listening to port 3000");
-});
